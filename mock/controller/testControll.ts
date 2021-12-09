9da5acd5c097e90af507523b4e25ae439ec1556a
吉文杰 <Jiwenjie97@gmail.com>
2021-12-09 19:58:54 +0800

    代码提交
    1. 提交部分注释。增加部分 package.json
    2. 增加部分测试代码

---
+++ mock/controller/testControll.ts
@@ -0,0 +1,23 @@
+/*
+ * @Description:table列表接口 (此处保留不删除，展示 faker.js 模拟数据框架得使用方法)
+ * @Author: 吉文杰
+ * @Date: 2021-11-20 09:46:46
+ * @LastEditors: 吉文杰
+ * @LastEditTime: 2021-11-21 17:31:24
+ */
+
+import { post, prefix, get } from "../requestDecorator";
+import faker from 'faker'
+
+
+@prefix('/syncCaseItems')
+export default class syncCaseItems {
+
+  @post('/num')
+  async num(ctx: any) {
+    console.log('ctx------------------', ctx.request.header)
+    return {
+        data: '啊是擦上擦擦是VS的'
+    }
+  }
+}
--- mock/mock.ts
+++ mock/mock.ts
@@ -8,6 +8,7 @@
 import Koa, { Context } from "koa";
 import koaBody from "koa-body";
 import koaRouter from "koa-router";
+import Session from "koa-session";    // 导入 session，准备增加登录请求验证部分内容
 import addRouter from "./router";
 import logger from "koa-logger";
 import log4js from "log4js";
@@ -17,12 +18,40 @@ import cors from "koa2-cors";
 import path from 'path';
 import { initWebSocket } from "./utils/Socket";
 
+// 准备添加 redis 使用，用户登录验证，cookie 和 session 部分内容
+// koa 设置 cookie 之后会自动加上 HttpOnly 属性，直接把用户信息存放在 Cookie 中不够安全，此时可以考虑使用 session 存放
+
+// tips: 可以把 sesioon 存在全局变量中，定期清理，也可以把 session 存在 redis 缓存中，设置一个到期时间，到期后从 redis 中就会
+// 获取不到该值，就认为登录状态失效。把 sessionId 存放在 Cookie 中实现。
+
+// 配置
+const session_signed_key = ["some-secret-jiwenjie"];  // 这个是配合signed属性的签名key
+const session_config = {
+    key: 'koa:sess',  /** cookie的key。 (默认是 koa:sess) */
+    maxAge: 9000,     /** session 过期时间，以毫秒ms为单位计算。*/
+    autoCommit: true, /** 自动提交到响应头。(默认是 true) */
+    overwrite: true,  /** 是否允许重写。(默认是 true) */
+    httpOnly: true,   /** 是否设置HttpOnly，如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。  (默认 true) */
+    signed: true,     /** 是否签名。(默认是 true) */
+    rolling: true,    /** 是否每次响应时刷新Session的有效期。(默认是 false) */
+    renew: false,     /** 是否在Session快过期时刷新Session的有效期。(默认是 false) */
+};
+
 // 实例化 Koa
+const port = 3300;
 const app = new Koa();
 app.use(cors());
+
+// 创建 session 
+const session = Session(session_config, app)
+app.keys = session_signed_key;
+// 使用中间件，注意有先后顺序
+app.use(session);
+
+// 实例化路由对象
 const router = new koaRouter();
 
-const port = 3300;
+// 实例化日志对象
 const log4 = log4js.getLogger();
 log4.level = "debug";
 
@@ -46,11 +75,12 @@ app.use(async (ctx, next) => {
 //  执行结果 handler 用来给每个响应对象包装响应码等
 app.use(ResultHandler());
 
-//加载路由
+// 加载路由
 addRouter(router);
 
-//启动路由
-app.use(router.routes()).use(router.allowedMethods());
+// 启动路由
+app.use(router.routes())
+   .use(router.allowedMethods());
 app.use(async (ctx: Context) => {
   log4.error(`404 ${ctx.message} : ${ctx.href}`);
   ctx.status = 404;
